597cae6f85e266b6ea9e144503afbf2527d0feb9
omatheusbrenno <155448437+omatheusbrenno@users.noreply.github.com>
2024-09-16 14:14:55 -0300

    Add files via upload

---
+++ higher_lower_game/game_data.py
@@ -0,0 +1,80 @@
+data = [
+   {
+      'name': 'Instagram',
+      'follower_count': '646',
+      'description': 'Social media platform',
+      'country': 'United States',
+   },
+   {
+      'name': 'Cristiano Ronaldo',
+      'follower_count': '608',
+      'description': 'Footballer',
+      'country': 'Portugal',
+   },
+   {
+      'name': 'Lionel Messi',
+      'follower_count': '487',
+      'description': 'Footballer',
+      'country': 'Argentina',
+   },
+   {
+      'name': 'Selena Gomez',
+      'follower_count': '428',
+      'description': 'Singer and actress',
+      'country': 'United States',
+   },
+   {
+      'name': 'Dwayne Johnson (The Rock)',
+      'follower_count': '398',
+      'description': 'Actor and wrestler',
+      'country': 'United States',
+   },
+   {
+      'name': 'Kylie Jenner',
+      'follower_count': '399',
+      'description': 'Media personality and businesswoman',
+      'country': 'United States',
+   },
+   {
+      'name': 'Ariana Grande',
+      'follower_count': '396',
+      'description': 'Singer and actress',
+      'country': 'United States',
+   },
+   {
+      'name': 'Kim Kardashian',
+      'follower_count': '378',
+      'description': 'Media personality and businesswoman',
+      'country': 'United States',
+   },
+   {
+      'name': 'Beyoncé',
+      'follower_count': '320',
+      'description': 'Singer and actress',
+      'country': 'United States',
+   },
+   {
+      'name': 'Khloe Kardashian',
+      'follower_count': '311',
+      'description': 'Media personality',
+      'country': 'United States',
+   },
+   {
+      'name': 'Justin Bieber',
+      'follower_count': '288',
+      'description': 'Singer',
+      'country': 'Canada',
+   },
+   {
+      'name': 'Kendall Jenner',
+      'follower_count': '292',
+      'description': 'Model and media personality',
+      'country': 'United States',
+   },
+   {
+      'name': 'Taylor Swift',
+      'follower_count': '276',
+      'description': 'Singer-songwriter',
+      'country': 'United States',
+   }
+]
